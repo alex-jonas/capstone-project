0dafb7487f4a83eb29aaffa5166dd659d5969354
Alexander Jonas <alex.jonas@gmail.com>
2020-11-24 15:57:10 +0100

    Add first api fetch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
-function App() {
-  return <div className="App"></div>
+import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import styled from 'styled-components/macro'
+
+export default function App() {
+  const [tracks, setTracks] = useState([])
+  useEffect(() => getTracks(), [])
+
+  function getTracks() {
+    fetch('http://wandergold.local/track')
+      .then((res) => res.json())
+      .then((data) => setTracks(data))
+      .catch((error) => console.error('Error:', error))
+  }
+
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          {tracks.map(({ id, description, title }) => (
+            <Track key={id}>
+              <p>{title}</p>
+              <p>{description}</p>
+            </Track>
+          ))}
+        </Route>
+        <Route exact path="/signup">
+          Registrieren!
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
-export default App
+const Track = styled.section`
+  width: 300px;
+  height: 150px;
+  background: #eee;
+`
