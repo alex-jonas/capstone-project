360f143f9597d0a54a0b2b6cd3e72d425db0fd08
Alexander Jonas <alex.jonas@gmail.com>
2020-11-25 10:22:05 +0100

    WIP

---
+++ src/GlobalStyles.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components/macro'
+
+export default createGlobalStyle`
+
+* {
+    box-sizing: border-box;
+    font-family: "Open Sans", sans-serif;
+}
+
+body {
+    margin:0;
+    font-size:112.5%;
+}
+
+:root {
+
+    --primary: #eee;
+}
+`
